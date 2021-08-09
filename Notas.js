c5914e8b0cbff63fe65af288a91107dd1eae2a55
Igor Mozetic <imozetic@blueedtech.com.br>
2021-08-09 14:58:45 -0300

    Alteração no código

--- Notas.js
+++ Notas.js
@@ -1,78 +1,121 @@
 function importarNotaAlunos() {
     let sheet = googleSheetConnectionNotas()
-    sheet.clear()
-    const header = ["ID Curso", "Nome do curso", "Data de criação atividade", "Data ultima atualizacao de envio atividade",
-        "Nome Externo", "ID aluno", "Nome do aluno", "Nota", "Nome Interno"];
-    sendDataToSheet(sheet, header)
-    let courses = getCourses()
-    courses.map((course) => {
-        let atividadesCurso = getCoursesWork(course.id);
-        let x = 1
-        atividadesCurso.forEach((atividade,) => {
-            let notas = getStudentGrade(atividade.id, course.id)
-            notas.forEach((nota) => {
-                let dataCriacao = new Date(atividade.creationTime).toLocaleDateString("pt-BR");
-                let dataEnvio = "";
-                if (nota.dataEnvio) {
-                    dataEnvio = new Date(nota.dataEnvio).toLocaleDateString("pt-BR");
-                }
-                else {
-                    dataEnvio = nota.dataEnvio;
-                }
-                let dados = [course.id, course.name, dataCriacao, dataEnvio, atividade.title, nota.userId, nota.nome, nota.nota, `P${x}`];
-                sendDataToSheet(sheet, dados)
-            });
-            x += 1
-        });
-    });
-}
-
-
-function getCoursesWork(courseID) {
-    let listaAtividadesCompleta = [],
-        options = {};
-    do {
-        let listaAtividadesRequest = Classroom.Courses.CourseWork.list(courseID, options);
-        Array.prototype.push.apply(listaAtividadesCompleta, listaAtividadesRequest.courseWork)
-        options.pageToken = listaAtividadesRequest.nextPageToken;
-    } while (options.pageToken);
-    return listaAtividadesCompleta.sort((a, b) => {
-        return sortAtividadeArr(a, b);
-    });
-}
-
-function filterUndefined(element) {
-    if (element) return true;
-    return false;
-}
-
-function sortAtividadeArr({ creationTime: data1 } = a, { creationTime: data2 } = b) {
-    return new Date(data1) - new Date(data2);
-}
-
-function getSubmitions(courseId, courseWorkId) {
-    let listaSubmitionsCompleta = [],
-        options = {};
-    do {
-        let listaSubmitionsRequest = Classroom.Courses.CourseWork.StudentSubmissions.list(courseId, courseWorkId, options);
-        Array.prototype.push.apply(listaSubmitionsCompleta, listaSubmitionsRequest.studentSubmissions);
-        options.pageToken = listaSubmitionsRequest.nextPageToken;
-    } while (options.pageToken);
-    return listaSubmitionsCompleta;
-}
-
-function getStudentGrade(atividadeCurso, cursoId) {
-    let submissions = getSubmitions(cursoId, atividadeCurso);
-    if (submissions) return submissions.map((submission) => {
-        let data = submission.updateTime ? submission.updateTime : submission.createdTime
-        let nomeAluno = Classroom.UserProfiles.get(submission.userId)
-        let nota = submission.assignedGrade ? submission.assignedGrade : ""
-        return { nota: submission.assignedGrade, dataEnvio: data, nome: nomeAluno.name.fullName, userId: nomeAluno.id }
-    }).filter((element) => { return filterUndefined(element) });
-}
-
-function googleSheetConnectionNotas() {
-    var sheetActive = SpreadsheetApp.openById("1s0kHkj9F269N3X3ii0u0JOk--DMhGmqbsrJvzArSXqQ");
-    var sheet = sheetActive.getSheetByName("Notas");
-    return sheet;
-}
+     sheet.clear()
+   const header = ["ID Curso",	"Nome do curso", "Data limite de entrega", "Data entrega atividade", "Status",	
+     "Nome Externo",	"ID aluno", "Nome do aluno", 	"Nota","Nome Interno"];
+     sendDataToSheet(sheet, header)
+   let courses = getCoursesAtivos()
+   courses.map((course)=> {
+     let atividadesCurso = getCoursesWork(course.id);
+     let x = 1
+     atividadesCurso.forEach((atividade,)=>{
+       let notas = getStudentGrade(atividade.id, course.id)
+       notas.forEach((nota) =>{
+         let dataCriacao = new Date(dueDateToDateString(atividade)).toLocaleString("pt-BR");
+         let dataEnvio = "";
+         if (nota.dataEnvio) {
+          dataEnvio = new Date(nota.dataEnvio).toLocaleString("pt-BR");
+         }
+         else {
+          dataEnvio = nota.dataEnvio;
+         }
+         let dados = [course.id, course.name,dataCriacao, dataEnvio, nota.status, atividade.title, nota.userId, nota.nome, nota.nota, `P${x}`];
+       //  console.log([course.id, course.name, dataCriacao, dataEnvio, atividade.title, nota.userId, nota.nome, nota.nota, `P${x}`])
+        sendDataToSheet(sheet, dados)
+       });
+       x += 1
+     });
+   });
+ }
+ 
+ 
+ function getCoursesWork(courseID){
+   let listaAtividadesCompleta = [],
+   options = {};
+   do {
+     let listaAtividadesRequest = Classroom.Courses.CourseWork.list(courseID, options);
+     Array.prototype.push.apply(listaAtividadesCompleta, listaAtividadesRequest.courseWork)
+     options.pageToken = listaAtividadesRequest.nextPageToken;
+   } while(options.pageToken);
+   return listaAtividadesCompleta.sort((a,b) => {
+     return sortAtividadeArr(a,b);
+   }).filter((atividade) => {
+     if(atividade.maxPoints){
+       return true;
+     }else {
+       return false;
+     }
+   });
+ }
+ 
+ function filterUndefined(element){
+   if(element) return true;
+   return false;
+ }
+ 
+ function sortAtividadeArr(a,b){  
+   let data1 = dueDateToDateString(a);
+   let data2 = dueDateToDateString(b);
+   return new Date(data1) - new Date(data2);
+ }
+ 
+ function getSubmitions(courseId, courseWorkId){
+   let listaSubmitionsCompleta = [],
+   options = {};
+   do {
+     let listaSubmitionsRequest = Classroom.Courses.CourseWork.StudentSubmissions.list(courseId, courseWorkId, options);
+     Array.prototype.push.apply(listaSubmitionsCompleta, listaSubmitionsRequest.studentSubmissions);
+     options.pageToken = listaSubmitionsRequest.nextPageToken;
+   } while(options.pageToken);
+   return listaSubmitionsCompleta;
+   
+ }
+ 
+ function getStudentGrade(atividadeCurso, cursoId){
+     console.log("CursoID = " +cursoId+ " | atividadeID = " +atividadeCurso)
+   let submissions = getSubmitions(cursoId, atividadeCurso);
+   if (submissions) return submissions.map((submission)=>{
+     console.log(submission)
+       let data = ""
+       if (submission.state != "CREATED" && submission.submissionHistory[1].stateHistory){
+         data = submission.submissionHistory[1].stateHistory.stateTimestamp
+       }
+       let nomeAluno = Classroom.UserProfiles.get(submission.userId)
+       let nota = submission.assignedGrade ? submission.assignedGrade : ""
+       let status = "";
+       if(data == ""){
+         status = "Pendente"
+       // } else if (submission.late){
+       //   status = "Entregue com atrasado"
+       } else{
+         status = "Entregue"
+       }
+       return {nota:submission.assignedGrade, dataEnvio:data, nome: nomeAluno.name.fullName, userId: nomeAluno.id, status: status}
+   }).filter((element) => {return filterUndefined(element)});
+ }
+ 
+ function googleSheetConnectionNotas(){
+   var sheetActive = SpreadsheetApp.openById("1s0kHkj9F269N3X3ii0u0JOk--DMhGmqbsrJvzArSXqQ");
+   var sheet = sheetActive.getSheetByName("Notas");
+   return sheet;
+ }
+ 
+ function dueDateToDateString(atividade){
+   let day = adicionarZeroAEsquerda(atividade.dueDate.day)
+   let month = adicionarZeroAEsquerda(atividade.dueDate.month)
+   let year = atividade.dueDate.year
+   let hours = adicionarZeroAEsquerda(atividade.dueTime.hours)
+   let minutes = "00";
+   if(atividade.dueTime.minutes)
+     minutes = adicionarZeroAEsquerda(atividade.dueTime.minutes)
+   let dateString = `${year}-${month}-${day}T${hours}:${minutes}:00.000Z`
+   return dateString;
+ }
+ 
+ function adicionarZeroAEsquerda(n) {
+   if (n <= 9) {
+     return "0" + n;
+   }
+   return n
+ }
+ 
--- index.js
+++ index.js
@@ -1,75 +1,98 @@
 function importClassroomToSheets() {
-    let sheet = googleSheetsConnection();
-    let data = new Date();
-    //sendDataToSheet(sheet, ["Data", "CursoId", "NomeCurso", "AlunoID", "NomeAluno", EmailAluno])
-    let listaCursos = getCourses();
-    listaCursos.forEach((curso) => {
-        let alunos = getStudents(curso.id);
-        if (alunos) {
-            //console.log(curso.name + " " + alunos.length)
-            alunos.forEach((aluno) => {
-                let sheetData = [data.toLocaleDateString("pt-BR"), curso.id, curso.name, aluno.id, aluno.nome, aluno.email]
-                sendDataToSheet(sheet, sheetData)
-            });
-        }
+  let sheet = googleSheetsConnection();
+  let data = new Date();
+  //sendDataToSheet(sheet, ["Data", "CursoId", "NomeCurso", "AlunoID", "NomeAluno", EmailAluno])
+  let listaCursos = getCourses();
+  listaCursos.forEach((curso)=>{
+  let alunos = getStudents(curso.id);
+  if (alunos){
+    //console.log(curso.name + " " + alunos.length)
+    alunos.forEach((aluno)=>{
+      let sheetData = [data.toLocaleDateString("pt-BR"),curso.id, curso.name, aluno.id, aluno.nome, aluno.email, curso.courseState]
+      sendDataToSheet(sheet, sheetData)
     });
+  }
+  });
 }
 
 //Função para obter todos os cursos ativos do usuário Educacional Blueedtech
-function getCourses() {
-    let listaClassroomCompleta = [];
-    let options = {};
+function getCourses(){
+  let listaClassroomCompleta = [];
+  let options = {};
 
-    //Enquanto houver um nextPageToken, solicita os proximos 30 cursos
-    do {
-        let listaClassroomRequest = Classroom.Courses.list(options);
-        //Adiciona os cursos do listaClassroomRequest no listaClassroomCompleta
-        Array.prototype.push.apply(listaClassroomCompleta, listaClassroomRequest.courses);
-        //Se houver um token, adiciona ao options
-        options.pageToken = listaClassroomRequest.nextPageToken;
-    } while (options.pageToken);
-    //Filtra por cursos ativos do usuário Educacional Blueedtech
-    return listaClassroomCompleta = listaClassroomCompleta.filter((classroom) => {
-        if (classroom.ownerId === "107558995070093037568" && classroom.courseState === "ACTIVE") {
-            return true;
-        } else {
-            return false;
-        }
-    });
+  //Enquanto houver um nextPageToken, solicita os proximos 30 cursos
+  do {
+    let listaClassroomRequest = Classroom.Courses.list(options);
+    //Adiciona os cursos do listaClassroomRequest no listaClassroomCompleta
+    Array.prototype.push.apply(listaClassroomCompleta,listaClassroomRequest.courses);
+    //Se houver um token, adiciona ao options
+    options.pageToken = listaClassroomRequest.nextPageToken;
+  } while (options.pageToken);
+  //Filtra por cursos ativos do usuário Educacional Blueedtech
+  return listaClassroomCompleta = listaClassroomCompleta.filter((classroom) => {
+    if(classroom.ownerId === "107558995070093037568"){
+      // if(classroom.ownerId === "107558995070093037568" && classroom.courseState === "ACTIVE"){
+      return true;
+    }else {
+      return false;
+    }
+  });
+}
+
+function getCoursesAtivos(){
+  let listaClassroomCompleta = [];
+  let options = {};
+
+  //Enquanto houver um nextPageToken, solicita os proximos 30 cursos
+  do {
+    let listaClassroomRequest = Classroom.Courses.list(options);
+    //Adiciona os cursos do listaClassroomRequest no listaClassroomCompleta
+    Array.prototype.push.apply(listaClassroomCompleta,listaClassroomRequest.courses);
+    //Se houver um token, adiciona ao options
+    options.pageToken = listaClassroomRequest.nextPageToken;
+  } while (options.pageToken);
+  //Filtra por cursos ativos do usuário Educacional Blueedtech
+  return listaClassroomCompleta = listaClassroomCompleta.filter((classroom) => {
+      if(classroom.ownerId === "107558995070093037568" && classroom.courseState === "ACTIVE"){
+      return true;
+    }else {
+      return false;
+    }
+  });
 }
 
 //Função para obter todos os estudantes de um curso
-function getStudents(courseID) {
-    const listaAlunosCompleta = [];
-    let options = {};
+function getStudents(courseID){
+  const listaAlunosCompleta = [];
+  let options = {};
 
-    //loop para buscar os estudantes enquanto houver um nextPageToken
-    do {
-        let alunosRequest = Classroom.Courses.Students.list(courseID, options)
-        //adiciona os estudantes do alunosRequest no listaAlunosCompleta
-        Array.prototype.push.apply(listaAlunosCompleta, alunosRequest.students)
-        //se houver um token, adiciona ao options
-        options.pageToken = alunosRequest.nextPageToken
-    } while (options.pageToken);
-    //verifica se existem alunos para o curso,
-    //Se sim, retorna a lista com objetos alunos
-    if (listaAlunosCompleta) {
-        return listaAlunosCompleta.map((aluno) => {
-            let id = aluno.profile.id;
-            let nome = aluno.profile.name.fullName;
-            let email = aluno.profile.emailAddress;
-            let dadosAlunos = { id, nome, email }
-            return dadosAlunos
-        });
-    }
+  //loop para buscar os estudantes enquanto houver um nextPageToken
+  do {
+    let alunosRequest = Classroom.Courses.Students.list(courseID,options)
+    //adiciona os estudantes do alunosRequest no listaAlunosCompleta
+    Array.prototype.push.apply(listaAlunosCompleta, alunosRequest.students)
+    //se houver um token, adiciona ao options
+    options.pageToken = alunosRequest.nextPageToken
+  } while (options.pageToken);
+  //verifica se existem alunos para o curso,
+  //Se sim, retorna a lista com objetos alunos
+  if(listaAlunosCompleta){
+    return listaAlunosCompleta.map((aluno)=>{
+      let id = aluno.profile.id;
+      let nome = aluno.profile.name.fullName;
+      let email = aluno.profile.emailAddress;
+      let dadosAlunos = {id, nome, email}
+      return dadosAlunos
+    });
+  }
 }
 
 //conecta ao sheet
-function googleSheetsConnection() {
-    return sheet = SpreadsheetApp.getActiveSheet()
+function googleSheetsConnection(){
+  return sheet = SpreadsheetApp.getActiveSheet()
 }
 
 //envia dados ao sheet
-function sendDataToSheet(sheet, data) {
-    sheet.appendRow(data);
+function sendDataToSheet(sheet, data){
+  sheet.appendRow(data);
 }
